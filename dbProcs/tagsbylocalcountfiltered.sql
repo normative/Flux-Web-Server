f4d63e80593aae3d0dd921d5a2c83c712dfde641
Denis Delorme <denis@smlr.is>
2013-09-20 13:37:27 -0400

    Fixups to stored proc.

--- dbProcs/tagsbylocalcountfiltered.sql
+++ dbProcs/tagsbylocalcountfiltered.sql
@@ -4,7 +4,7 @@
 DROP FUNCTION tagsbylocalcount(lat double precision, lon double precision, radius double precision, maxcount integer);
 */
 
-CREATE OR REPLACE FUNCTION tagsbylocalcountfiltered(lat double precision, lon double precision, radius double precision
+CREATE OR REPLACE FUNCTION tagsbylocalcountfiltered(lat double precision, lon double precision, radius double precision,
 							minalt double precision, maxalt double precision,
 							mintime timestamp, maxtime timestamp,
 							taglist text,
@@ -17,6 +17,7 @@ CREATE OR REPLACE FUNCTION tagsbylocalcountfiltered(lat double precision, lon do
 RETURNS TABLE (tagtext varchar, count bigint)
 AS
 $$
+DECLARE
 	tagset text[];
 	tagarraylen integer;
 	userset text[];
@@ -47,39 +48,6 @@ BEGIN
 		maxtime = 'infinity'::timestamp;
 	END IF;
 		
-
-RETURN QUERY
-	SELECT	DISTINCT(i.*)
-	FROM	
-		(SELECT * FROM buildboundingbox(lat, lon, radius) FETCH FIRST 1 ROWS ONLY) as bb,
-		images i
-		LEFT OUTER JOIN images_tags imt ON i.id = imt.image_id
-		LEFT OUTER JOIN tags t ON (imt.tag_id = t.id)
-		JOIN categories c ON i.category_id = c.id
-		JOIN users u ON i.user_id = u.id
-	WHERE	( 
-		-- location
-			((skiploc) OR
-			 ((i.best_latitude > bb.minlat) AND (i.best_latitude < bb.maxlat) AND
-			 (i.best_longitude > bb.minlon) AND (i.best_longitude < bb.maxlon))
-			)
-		-- altitude
-		AND	(((minalt IS NULL) OR (i.best_altitude >= minalt))
-		    AND	 ((maxalt IS NULL) OR (i.best_altitude <= maxalt))
-			)
-		-- time
---		AND	(i.time_stamp BETWEEN mintime AND maxtime)
-		AND	(((mintime IS NULL) OR (i.time_stamp >= mintime))
-		    AND	 ((maxtime IS NULL) OR (i.time_stamp <= maxtime))
-			)
-		-- tags
-		 AND 	((tagarraylen IS NULL) OR (tagarraylen = 0) OR (t.tagtext = ANY (tagset)))
-		-- users
-		 AND 	((userarraylen IS NULL) OR (userarraylen = 0) OR (u.nickname = ANY (userset)))
-		-- categories
-		 AND 	((catarraylen IS NULL) OR (catarraylen = 0) OR (c.cat_text = ANY (catset)))
-		 )
-	ORDER by i.id;
 
 RETURN QUERY	
 	SELECT	t.tagtext, count(imt.image_id) AS "count"
@@ -114,7 +82,7 @@ RETURN QUERY
 		 )
 	GROUP BY t.tagtext
 	ORDER BY "count" DESC, MAX(i.time_stamp), t.tagtext
-	LIMIT maxrows;
+	LIMIT maxcount;
 
 END;
 $$ LANGUAGE plpgsql;
