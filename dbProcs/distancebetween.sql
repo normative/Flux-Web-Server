20552f6b505685e8103a976432aeb800ce84db4b
Denis Delorme <denis@smlr.is>
2013-09-27 11:58:37 -0400

    Function and sql test code for double precision testing.

---
+++ dbProcs/distancebetween.sql
@@ -0,0 +1,42 @@
+﻿
+-- uses the Haversine formula to build a rectangular box of dimension (2 X radius) X (2 X radius) centered on the provided latitude and longitude
+
+CREATE OR REPLACE FUNCTION distancebetween(lat1 double precision, lon1 double precision, lat2 double precision, lon2 double precision)
+RETURNS double precision
+AS $$
+DECLARE
+	lat0 double precision;
+	a  double precision;
+	f  double precision;
+	e2  double precision;
+	R1  double precision;
+	R2  double precision;
+	oneMeterLat  double precision;
+	oneMeterLon  double precision;
+	latdif double precision;
+	londif double precision;
+
+BEGIN
+
+  lat0 = lat1 / 180.0 * pi();	-- convert to radians
+
+  a = 6378137.0;
+  e2 = 0.0066943799901413169964451404764132282816;
+
+  R1 = a * (1.0 - e2) / power((1.0 - e2 * power(sin(lat0), 2.0)), (3.0 / 2.0));
+  R2 = a / sqrt(1 - e2 * power(sin(lat0), 2.0));
+
+  -- calc dlat and dlon (in degrees) for dN = dE = 1m
+  oneMeterLat = (1.0 / R1) * (180.0 / pi());
+  oneMeterLon = (1.0 / (R2 * cos(lat0))) * (180.0 / pi());
+
+  latdif = (lat2 - lat1) / oneMeterLat;
+  londif = (lon2 - lon1) / oneMeterLon;
+  
+RETURN 
+	
+	sqrt((latdif * latdif) + (londif * londif)) as distance;
+
+END;
+$$ LANGUAGE plpgsql;
+
---
+++ dbProcs/doubleDistanceTests.sql
@@ -0,0 +1,73 @@
+﻿select distancebetween(43.65339, -79.40659, 43.65338, -79.40661);
+
+select distancebetween(43.65338, -79.40661, 43.65339, -79.40661);
+
+select distancebetween(43.3248921158196, -79.8129805550722, ((43.3248921158196::float4)::float8), ((-79.8129805550722::float4)::float8)) 
+
+
+select (-79.8129805550722::float4)
+
+--SELECT i.id, i.best_longitude, round(i.best_longitude::numeric, 5) as chopped, 
+--		distancebetween(i.best_latitude, i.best_longitude, (((i.best_latitude * 100000.0)::integer)::float8)/100000.0::float8, (((i.best_longitude * 100000.0)::integer)::float8)/100000.0::float8) as dubdistance,
+
+
+SELECT i.id, i.best_longitude, round(i.best_longitude::numeric, 4) as chopped, 
+		distancebetween(i.best_latitude, i.best_longitude, round(i.best_latitude::numeric, 4), round(i.best_longitude::numeric, 4)) as floatdistance
+FROM images i
+-- WHERE i.id >= 495
+-- WHERE (i.id < 495) AND (i.best_longitude > -100.0)
+-- WHERE best_longitude < -100.0
+ WHERE (i.id >= 196) AND (i.id < 346) AND (i.best_longitude < -110.0)
+-- WHERE (i.id < 495)
+ORDER BY floatdistance DESC;
+
+
+SELECT * from images i where i.id between 223 and 346 ORDER by i.id DESC;
+
+
+
+select (((12.34567890 * 100000.0)::integer)::float8)/100000.0::float8
+
+-- local, double
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, round(i.best_latitude::NUMERIC, 5), round(i.best_longitude::NUMERIC, 5))) as avgdist
+FROM images i
+WHERE (i.id >= 495) AND (i.id < 509)
+
+-- west coast, float
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, round(i.best_latitude::NUMERIC, 4), round(i.best_longitude::NUMERIC, 4))) as avgdist
+FROM images i
+WHERE (i.id >= 196) AND (i.id < 346) AND (i.best_longitude < -110.0)
+
+-- local, float
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, round(i.best_latitude::NUMERIC, 5), round(i.best_longitude::NUMERIC, 5))) as avgdist
+FROM images i
+WHERE i.id < 223
+
+-- west coast, double
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, round(i.best_latitude::NUMERIC, 4), round(i.best_longitude::NUMERIC, 4))) as avgdist
+FROM images i
+WHERE (i.id >= 509) AND (i.id < 1000)
+
+select id, best_longitude from images where best_longitude < -110.0 order by id
+
+
+-- test database - first float/double image @ id=280, local
+select * from images where id >= 280
+
+-- local, mixed
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, i.raw_latitude, i.raw_longitude)) as avgdist
+FROM images i
+WHERE (i.id >= 280) AND (i.id < 292)
+
+-- west coast, mixed
+SELECT COUNT(i.id) as count, AVG(distancebetween(i.best_latitude, i.best_longitude, i.raw_latitude, i.raw_longitude)) as avgdist
+FROM images i
+WHERE (i.id >= 292)
+
+
+select id, raw_latitude, best_latitude, raw_longitude, best_longitude, round(distancebetween(best_latitude, best_longitude, raw_latitude, raw_longitude)::numeric, 4) as distance
+from images where id >= 280 
+order by distance DESC;
+
+
+
