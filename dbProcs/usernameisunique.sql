090fceed8630d5000e7234ba003f942cbfb2cb07
Denis Delorme <denis@smlr.is>
2013-12-12 09:01:11 -0500

    Add usernameisunique sql stored procedure to return whether a given username is unique and to suggest a unique name based on the provided username.  Previous checkin also includes code for returning an image list for a specific userid.

---
+++ dbProcs/usernameisunique.sql
@@ -0,0 +1,45 @@
+﻿
+/*
+DROP FUNCTION usernameisunique(uname text)
+*/
+
+/*
+CREATE TYPE unique_suggestion AS (isunique boolean, suggested_name varchar);
+*/
+
+CREATE OR REPLACE FUNCTION usernameisunique(uname text)
+RETURNS SETOF unique_suggestion
+AS $$
+DECLARE
+	suggested varchar;
+	c integer;
+	suffix integer;
+	isunique boolean;
+
+BEGIN
+
+	suggested := uname;
+
+	SELECT COUNT(id) INTO c
+	FROM users
+	WHERE username = suggested;
+
+	suffix := 0;
+	isunique := (c = 0);
+
+	WHILE c > 0 LOOP
+		suffix := suffix + 1;
+		
+		suggested = uname || btrim(('' || suffix));
+		
+		SELECT COUNT(id) INTO c
+		FROM users
+		WHERE username = suggested;
+		
+	END LOOP;
+
+RETURN QUERY
+	SELECT isunique AS "isunique", suggested AS "suggested_name";
+	
+END;
+$$ LANGUAGE plpgsql;
