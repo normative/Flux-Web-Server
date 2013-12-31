66eda1ef332ce6dcfc3d83895b9998765f1448bf
Denis Delorme <denis@smlr.is>
2013-12-31 08:46:58 -0500

    Fix processor class name so that it can be found

--- lib/paperclip_processors/feature_extractor.rb
+++ lib/paperclip_processors/feature_extractor.rb
@@ -1,11 +1,12 @@
+#module Paperclip
 # Handles extracting features from images that are uploaded.
-class Feature_extractor < Paperclip::Processor
+class FeatureExtractor < Paperclip::Processor
 
   # Creates a Feature_extractor object set to work on the +file+ given. It
   # will attempt to extract features from the supplied image
 
   def initialize(file, options = {}, attachment = nil)
-    logger.debug "Into Feature_extractor#initialize"
+#    logger.debug "Into FeatureExtractor#initialize"
     super
     @file                = file
     @current_format      = File.extname(@file.path)
@@ -15,10 +16,10 @@ class Feature_extractor < Paperclip::Processor
   # Performs the extraction of features from the +file+ into an extraction file. Returns the Tempfile
   # that contains the features.
   def make
-    logger.debug "Into Feature_extractor#make"
+#    logger.debug "Into FeatureExtractor#make"
     src = @file
 #      dst = Tempfile.new([@basename, @format ? ".#{@format}" : ''])
-    dst = Tempfile.new([@basename, ".feq"])
+    dst = Tempfile.new([@basename, ".fex"])
     dst.binmode
 
     begin
@@ -28,11 +29,11 @@ class Feature_extractor < Paperclip::Processor
 
       parameters = parameters.flatten.compact.join(" ").strip.squeeze(" ")
 
-      success = extract(parameters, :source => File.expand_path(src.path), :dest => File.expand_path(dst.path))
+      success = extract(parameters, :source => "#{File.expand_path(src.path)}[0]", :dest => File.expand_path(dst.path))
     rescue Cocaine::ExitStatusError => e
       raise Paperclip::Error, "There was an error processing the extraction for @basename"
     rescue Cocaine::CommandNotFoundError => e
-      raise Paperclip::Errors::CommandNotFoundError.new("Could not run the `feature_extract` command. Please install ImageMagick.")
+      raise Paperclip::Errors::CommandNotFoundError.new("Could not run the `feature_extract` command. Please install.")
     end
 
     dst
@@ -45,3 +46,4 @@ class Feature_extractor < Paperclip::Processor
   end
 
 end
+#end
