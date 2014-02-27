733cb477fd076a937f9309b7c7fc14199545fb75
Denis Delorme <denis@smlr.is>
2014-02-27 16:18:40 -0500

    Twitter contact import now complete and working for all cases tested (not too much data in db so difficult to fully test)

--- app/controllers/aliases_controller.rb
+++ app/controllers/aliases_controller.rb
@@ -73,13 +73,13 @@ class AliasesController < ApplicationController
     # It also returns a simple Hash, which is significantly more efficient than a
     # full blown ActiveRecord model.
     results = ActiveRecord::Base.connection.select_all(query)
-    results = results.sort{|x, y| x[:id] <=> y[:id]}
+    results = results.sort{|x, y| x["user_id"] <=> y["user_id"]}
 
     # filter out duplicate flux user ids - pick only the first since it doesn't really matter which...
     uniqresults = Array.new
     lastid = -1
     if (results.size > 0)
-      results.rows.each do |r|
+      results.each do |r|
         if r[0] != lastid
           uniqresults << r
           lastid = r[0]
@@ -88,26 +88,17 @@ class AliasesController < ApplicationController
     end
 
     # now merge two lists based on alias_name and add the profile image URL into the hash...
-    uniqresults = uniqresults.sort{|x, y| x[:alias_name] <=> y[:alias_name]}
+    uniqresults = uniqresults.sort{|x, y| x["alias_name"].to_s <=> y["alias_name"].to_s}
     c_idx = 0   
     newrows = Array.new
     if (uniqresults.size > 0)
-      uniqresults.rows.each do |r|
+      uniqresults.each do |r|
         if (service_id == 1)
           # email contacts...
         elsif (service_id == 2)
           # Twitter contacts...
-          if (r[:alias_name] == contacts[c_idx].username)
-            if (contacts[c_idx].profile_image_uri?)
-              r[:profile_pic_URL] = contacts[c_idx].profile_image_uri.to_s
-            else
-              r[:profile_pic_URL] = ''
-            end
-            r[:display_name] = contacts[c_idx].name    
-          end
-          
-          while (contacts[c_idx].username <= r[:alias_name]) && (c_idx < contacts.size) do
-            if (contacts[c_idx].username != r[:alias_name])
+          while (c_idx < contacts.size) && (contacts[c_idx].username.to_s < r["alias_name"].to_s)  do
+            if (contacts[c_idx].username != r["alias_name"])
               # add rows to something to add into r later...
               nr = {alias_name: contacts[c_idx].username, profile_pic_URL: contacts[c_idx].profile_image_uri.to_s,
                       display_name: contacts[c_idx].name,
@@ -116,10 +107,40 @@ class AliasesController < ApplicationController
             end
             c_idx = c_idx + 1
           end
+
+          if (c_idx < contacts.size) && (r["alias_name"].to_s == contacts[c_idx].username.to_s)
+            nr = {alias_name: r["alias_name"],
+                    display_name: contacts[c_idx].name,
+                    profile_pic_URL: '',
+                    user_id: r["user_id"], username: r["username"], 
+                    am_follower: r["am_follower"], is_following: r["is_following"], friend_state: r["friend_state"]}
+            if (contacts[c_idx].profile_image_uri?)
+              nr[:profile_pic_URL] = contacts[c_idx].profile_image_uri.to_s
+            end
+            newrows << nr
+            c_idx = c_idx + 1
+          end
+          
         elsif (service_id == 3)
           # Facebook
         end
       end
+      
+      # now do the rest in the contacts list...
+      if (service_id == 1)
+        # email contacts...
+      elsif (service_id == 2)
+        # Twitter contacts...
+        while (c_idx < contacts.size)  do
+          # add rows to something to add into r later...
+          nr = {alias_name: contacts[c_idx].username, profile_pic_URL: contacts[c_idx].profile_image_uri.to_s,
+                  display_name: contacts[c_idx].name,
+                  user_id: 0, username: '', am_follower: 0, is_following: 0, friend_state: 0}
+          newrows << nr
+          c_idx = c_idx + 1
+        end
+      end      
+      
     elsif (contacts.size > 0)
       contacts.each do |c|
         nr = {alias_name: c.username, profile_pic_URL: c.profile_image_uri.to_s,
@@ -130,15 +151,15 @@ class AliasesController < ApplicationController
     end
 
     # add in the new rows...
-    uniqresults = uniqresults + newrows
+    finalresults = newrows
     
     # sort by alias_name
-    uniqresults = uniqresults.sort{|x, y| x[:alias_name] <=> y[:alias_name]}
+    finalresults = finalresults.sort{|x, y| x[:alias_name] <=> y[:alias_name]}
                       
     respond_to do |format|
 #      format.html # show.html.erb
 #      format.json { render json: @aliases }
-      format.json { render json: uniqresults }
+      format.json { render json: finalresults }
     end
   end
 
