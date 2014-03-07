de6d1ceaf935e3ad23cac42294ee8ffd3f7ff8b9
Denis Delorme <denis@smlr.is>
2014-03-07 09:34:53 -0500

    Add APNS notification to the user when a user is followed

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -134,6 +134,9 @@ class ConnectionsController < ApplicationController
       end
     end
 
+    # notify the user being followed...    
+    ApnsClient.sendmessage(@connection.user_id, connections_id, 3)
+
     respond_to do |format|
       if @connection.save
  #       format.html { redirect_to @connection, notice: 'Connection was successfully created.' }
