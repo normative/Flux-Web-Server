508d5196be34b0226296449eae9de247607e4b50
Denis Delorme <denis@smlr.is>
2014-03-13 21:35:05 -0400

    Updates to connections model and controller, and derivative route updates to implement refactor changes based on new friend/follow paradigm.

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -5,12 +5,11 @@ class Connection < ActiveRecord::Base
 
   validates_presence_of :user_id, :connections_id
 
-  # connection type: 1 = follower, 2 = friend, 0 = no connection (record can be deleted)
-  # connection status: 1 = request pending, 2 = request accepted, 4 = request rejected
+  # connection status: 1 = request pending, 2 = request accepted
   
-  def self.getfriends myid
-    select("*").from("getfriends('#{myid}')") 
-  end
+#  def self.getfriends myid
+#    select("*").from("getfriends('#{myid}')") 
+#  end
 
   def self.getfollowing myid, dir
     # dir = 0 => i am following...
@@ -18,8 +17,12 @@ class Connection < ActiveRecord::Base
     select("*").from("getfollowers('#{myid}', #{dir})") 
   end
   
-  def self.getfriendinvites myid
-    select("*").from("getfriendinvites('#{myid}')") 
+#  def self.getfriendinvites myid
+#    select("*").from("getfriendinvites('#{myid}')") 
+#  end
+
+  def self.getfollowerrequests myid
+    select("*").from("getfollowerinvites('#{myid}')") 
   end
 
 end
