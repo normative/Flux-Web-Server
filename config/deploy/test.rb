f3685e4ae310e3ffd032e83de432655dd24b2276
Denis Delorme <denis@smlr.is>
2014-03-27 22:25:30 -0400

    Add test environment to map to AWS micro

---
+++ config/deploy/test.rb
@@ -0,0 +1,7 @@
+set :deploy_to, '/home/ubuntu/flux_web/'
+set :rails_env, 'test'
+set :user, 'ubuntu'
+set :use_sudo, false
+set :rvm_ruby_string, 'ruby-1.9.3-p484'
+set :ssh_options, { forward_agent: true, keys: File.join(ENV["HOME"], ".ssh", "SMLR_primary_NV.pem") }
+server "54.221.222.71", :web, :app, :db, primary: true
