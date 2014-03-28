72481472f25328f579c36a1e1d216c43048b7d62
Denis Delorme <denis@smlr.is>
2014-03-28 10:20:00 -0400

    Add debug log message dumping out the errors on failed image upload.

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,10 +7,10 @@ set :default_stage, "testing"
 
 set :application, "SMLR Flux"
 set :repository,  "git@bitbucket.org:smlrteam/smlr_flux_webserver.git"
-set :branch, 'master'
+set :branch, 'uploadTesting'
 
 # if you want to clean up old releases on each deploy uncomment this:
-after "deploy:restart", "deploy:cleanup"
+#after "deploy:restart", "deploy:cleanup"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
