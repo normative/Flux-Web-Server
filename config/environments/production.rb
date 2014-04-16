9db504dc4e973292799346e4098f20d0ec83fd24
Denis Delorme <denis@smlr.is>
2014-04-16 14:40:13 -0400

    Prep (production) and update (staging) configuration to setup a link via https for password reset.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,8 @@ Web::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => '54.221.254.230' }
+  #config.action_mailer.default_url_options = { :host => 'production.flux.smlr.is', :protocol => 'https' }
+  config.action_mailer.default_url_options = { :host => 'production.flux.smlr.is' }
 
   config.action_mailer.delivery_method = :smtp
 
