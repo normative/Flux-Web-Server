44acbe08525954654e538cee859376d4d3e4d973
Andy MacDonald <andy@normative.com>
2016-09-28 23:07:19 -0400

    staging settings

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,9 @@
-set :deploy_to, '/home/ubuntu/flux_web/'
+set :deploy_to, '/home/ubuntu/flux_app/'
 set :rails_env, 'staging'
-set :user, 'ubuntu'
+set :user, 'ec2-user'
 set :use_sudo, false
 set :rvm_ruby_string, 'ruby-1.9.3-p484'
-set :ssh_options, { forward_agent: true, keys: File.join(ENV["HOME"], ".ssh", "SMLR_primary_NV.pem") }
-server "54.83.61.163", :web, :app, :db, primary: true
+set :rvm_type, :user
+# set :ssh_options, { forward_agent: true, keys: File.join(ENV["HOME"], ".ssh", "SMLR_primary_NV.pem") }
+set :ssh_options, { forward_agent: true }
+server "ec2-54-70-65-128.us-west-2.compute.amazonaws.com", :web, :app, :db, primary: true
