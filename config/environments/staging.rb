2b72bccd0536c438a20955e39c8b02eef47020cd
Andy MacDonald <andy@normative.com>
2016-10-06 16:20:53 -0400

    adding mailgun support

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -52,7 +52,7 @@ Web::Application.configure do
 
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'fluxapp.normative.com', :protocol => 'https' }
+  # config.action_mailer.default_url_options = { :host => 'fluxapp.normative.com', :protocol => 'https' }
 
   config.action_mailer.delivery_method = :smtp
 
