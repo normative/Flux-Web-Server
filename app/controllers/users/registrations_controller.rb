4a2d48f7426e0ea108f692538b2777a39a76f84f
Andy MacDonald <andy@normative.com>
2016-10-06 16:31:00 -0400

    removes remote loggin

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,16 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   def create
-    puts 'CREATING REGISTRATION'
     build_resource(resource_params)
 
     if resource.save
-      puts 'SAVED'
       render json: resource.as_json(auth_token: resource.authentication_token, username: resource.username), status: 201
       return
     else
       clean_up_passwords resource
-      puts 'COULD NOT SAVE RESOURCE USER'
       warden.custom_failure!
       render json: resource.errors, status: 422
       return
