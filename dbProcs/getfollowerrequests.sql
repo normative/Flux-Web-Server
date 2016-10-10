2f9380de9c88b59f5804b13269763dfd9bf2d3aa
Andy MacDonald <andy@normative.com>
2016-10-10 19:36:04 -0400

    updating follower response stored proc

--- dbProcs/getfollowerrequests.sql
+++ dbProcs/getfollowerrequests.sql
@@ -8,21 +8,20 @@ http://127.0.0.1:3101/aliases/getfollowerrequests?auth_token=AoSZmitKx7Mq8dkXd9Q
 
 CREATE OR REPLACE FUNCTION getfollowerrequests(mytoken text)
 
-RETURNS TABLE(id integer, username varchar, since timestamp)
+RETURNS TABLE(id integer, username varchar)
 AS $$
 DECLARE
 	my_id integer;
 
 	r RECORD;
 BEGIN
-	SELECT u.id INTO my_id 
-	FROM users AS u 
+	SELECT u.id INTO my_id
+	FROM users AS u
 	WHERE authentication_token = mytoken;
-	
+
 RETURN QUERY
-	SELECT	u.id AS id, 
-		u.username AS username,
-		c.updated_at AS since
+	SELECT	u.id AS id,
+		u.username AS username
 	FROM	users u
 		INNER JOIN connections c ON ((c.user_id = u.id) AND (c.connections_id = my_id) AND (c.following_state = 1));
 END;
