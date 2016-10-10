65780553f07ac54b5314c176bfd053ff9ecc979d
Andy MacDonald <andy@normative.com>
2016-10-10 19:27:44 -0400

    fixing invite routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Web::Application.routes.draw do
 #    resources :sessions, :constraints => { :protocol => "https" }
 #  end
 #  Or if you need to force SSL for multiple routes:
-#  
+#
 #  MyApplication::Application.routes.draw do
   get "content_flags/new"
-#    scope :constraints => { :protocol => "https" } do 
+#    scope :constraints => { :protocol => "https" } do
 #      # All your SSL routes.
 #    end
 #  end
- 
-#  scope :constraints => { :protocol => "https" } do   
-    devise_for :users, controllers: { sessions: "users/sessions", 
-                                      passwords: "users/passwords", 
+
+#  scope :constraints => { :protocol => "https" } do
+    devise_for :users, controllers: { sessions: "users/sessions",
+                                      passwords: "users/passwords",
                                       confirmations: "users/confirmations",
-                                      registrations: "users/registrations" 
+                                      registrations: "users/registrations"
                                     }
 
     devise_scope :user do
@@ -33,7 +33,7 @@ Web::Application.routes.draw do
         get 'followers',     :to => "connections#followers"
         get 'followerrequests', :to => "connections#followerrequests"
         put 'updateapnstoken'
-        put 'invitetoflux'
+        put 'invitetoflux', :to =>"users#invitetoflux"
       end
       member do
         get 'profile'
@@ -42,12 +42,12 @@ Web::Application.routes.draw do
 #        get 'validateemail'
       end
     end
-  
+
     resources :images do
       collection do
         get 'filtered'  # more comprehensive filtering of queries
         get 'filteredcontent' # filtered but returning only lat, lon, alt, imageid, type
-        get 'filteredimgcounts'      # returns total image count, count of my images, count of friends images and count of following images 
+        get 'filteredimgcounts'      # returns total image count, count of my images, count of friends images and count of following images
         get 'getimagelistforuser'    # returns list of [id, description] pairs, ordered by time_stamp [DESC] with the given userid
         get 'nuke'
         put 'setprivacy'
@@ -64,7 +64,7 @@ Web::Application.routes.draw do
         patch 'setdescription'
       end
     end
-  
+
     resources :cameras do
       collection do
         get 'lookupbydevid'  # camera that matches specified deviceid
@@ -73,7 +73,7 @@ Web::Application.routes.draw do
         get 'camera'
       end
     end
-  
+
     resources :tags do
       collection do
         get 'localbycountfiltered'
@@ -84,7 +84,7 @@ Web::Application.routes.draw do
         get 'tag'
       end
     end
-    
+
     resources :aliases do
       collection do
         get 'importcontacts'
@@ -94,7 +94,7 @@ Web::Application.routes.draw do
         get 'alias'
       end
     end
-    
+
     resources :connections do
       collection do
         post 'follow'
@@ -104,8 +104,8 @@ Web::Application.routes.draw do
       member do
       end
     end
-#  end    
-  
+#  end
+
 #  resources :image_matches do
 #    collection do
 #    end
