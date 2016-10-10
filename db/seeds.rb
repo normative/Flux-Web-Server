8da2107316196db2b8ed2a4d4fb3e47bd0132f43
Andy MacDonald <andy@normative.com>
2016-10-10 13:24:06 -0400

    more proc errors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 include FactoryGirl::Syntax::Methods
 
-create_list(:user, 10)
-create_list(:camera, 10)
-create_list(:category, 3)
+# create_list(:user, 10)
+# create_list(:camera, 10)
+# create_list(:category, 3)
