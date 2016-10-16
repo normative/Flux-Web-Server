4875182dee6a2258a1a53526fcd2dedd2515c236
Andy MacDonald <andy@normative.com>
2016-10-16 01:20:27 -0400

    removing cancan access for images

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,7 +2,6 @@ class ImagesController < ApplicationController
 
   before_filter :authenticate_user_from_token!
   before_filter :authenticate_user!, except: [ :renderimage ]
-  authorize_resource
 
   # GET /images
   # GET /images.json
