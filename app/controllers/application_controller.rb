e2a4d5789ff7e65f242634a933b0a910b944ea37
Andy MacDonald <andy@normative.com>
2016-10-16 01:11:26 -0400

    turning off auth for image routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,22 +3,22 @@ class ApplicationController < ActionController::Base
   before_filter {
     request.env['devise.skip_storage'] = true # turn off cookies
   }
-  
+
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up).push :username, :email, :password, :name, :bio
     devise_parameter_sanitizer.for(:sign_in).push :username, :password
   end
-  
+
   private
 
   def authenticate_user_from_token!
     auth_token = params[:auth_token].presence
     # temporary fix until app can be cleaned up properly
-    if auth_token[0] == "'"
-      auth_token.delete! "'"
-    end
+    # if auth_token[0] == "'"
+    #   auth_token.delete! "'"
+    # end
     user = auth_token && User.find_by_authentication_token(auth_token)
 
     if user
