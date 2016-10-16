5b95100dd1ac979d4c4260d44c25de8c22ba1fc2
Andy MacDonald <andy@normative.com>
2016-10-15 23:20:44 -0400

    trying to get the right path

--- app/models/image.rb
+++ app/models/image.rb
@@ -159,7 +159,7 @@ class Image < ActiveRecord::Base
     request.add_field("Authorization", "Bearer ICgn5t1EZkhRuPbH4mO2on0D7h7dZO")
     request.add_field("Content-Type","application/json")
 
-    path = self.image.historical.path(:oriented)
+    path = self.image.expiring_url(500, :oriented)
     if(path.nil?)
       path = self.image.expiring_url(500, :oriented)
     end
