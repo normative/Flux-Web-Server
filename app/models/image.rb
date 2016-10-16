f2a38d7e820ad2fb679bc2c0f4a5ce389ad5f158
Andy MacDonald <andy@normative.com>
2016-10-16 10:17:38 -0400

    testing

--- app/models/image.rb
+++ app/models/image.rb
@@ -180,9 +180,10 @@ class Image < ActiveRecord::Base
         Delayed::Worker.logger.debug(output)
         output['data']['concepts'].each do |concept|
           Delayed::Worker.logger.debug(concept)
-          if concept['value'] > 0.5
-            Delayed::Worker.logger.debug("CREATING TAG FOR #{concept['name']}")
-            @tag = Tag.find_or_create_by(tagtext: concept['name'])
+          con = JSON.parse(concept)
+          if con['value'] > 0.5
+            Delayed::Worker.logger.debug("CREATING TAG FOR #{con['name']}")
+            @tag = Tag.find_or_create_by(tagtext: con['name'])
             self.tags << @tag
           end
         end
@@ -190,5 +191,5 @@ class Image < ActiveRecord::Base
       self.save
     end
   end
-  handle_asynchronously :generate_tags, :run_at => Proc.new { 5.seconds.from_now }
+  handle_asynchronously :generate_tags, :run_at => Proc.new { 2.seconds.from_now }
 end
