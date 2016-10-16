c7b765296d9dff34926642d09363873322e187d2
Andy MacDonald <andy@normative.com>
2016-10-16 10:49:21 -0400

    testing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,16 +3,16 @@ class Tag < ActiveRecord::Base
 
   has_and_belongs_to_many :images
 
-  validates_presence_of :tag_text
+  validates_presence_of :tagtext
 
-  def to_s; tag_text; end
+  def to_s; tagtext; end
 
   def self.getlocalbycount lat, lng, radius, maxcount
-    select("*").from("tagsbylocalcount(#{lat}, #{lng}, #{radius}, #{maxcount})") 
+    select("*").from("tagsbylocalcount(#{lat}, #{lng}, #{radius}, #{maxcount})")
   end
 
   def self.getlocalbycountfiltered myid, lat, lng, radius, minalt, maxalt, mintime, maxtime, taglist, userlist, mypics, followingpics, maxcount
-    select("*").from("tagsbylocalcountfiltered('#{myid}', #{lat}, #{lng}, #{radius}, #{minalt}, #{maxalt}, #{mintime}, #{maxtime}, #{taglist}, #{userlist}, #{mypics}, #{followingpics}, #{maxcount})") 
+    select("*").from("tagsbylocalcountfiltered('#{myid}', #{lat}, #{lng}, #{radius}, #{minalt}, #{maxalt}, #{mintime}, #{maxtime}, #{taglist}, #{userlist}, #{mypics}, #{followingpics}, #{maxcount})")
   end
-  
+
 end
